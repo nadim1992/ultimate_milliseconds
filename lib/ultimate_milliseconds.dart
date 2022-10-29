7a98dd93ee4575fe8daccc36726ad3ef091479cb
Nadim <nadim0988@gmail.com>
2022-10-29 13:39:20 +0600

    cleanup files

--- lib/ultimate_milliseconds.dart
+++ lib/ultimate_milliseconds.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library ultimate_milliseconds;
 
 export 'src/ultimate_milliseconds_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
